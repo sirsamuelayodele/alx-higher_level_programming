9d42e523f29e803dc7b6552bdc4d5b4f2d95283e
root <asatechafrica@gmail.com>
2024-01-04 08:32:20 -0800

    I sometimes suffer from insomnia and when I cant fall asleep I play what i call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # Project name: 2-print_alphabet.py
 # Author: sirsamuelayodele of ASA
-
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
