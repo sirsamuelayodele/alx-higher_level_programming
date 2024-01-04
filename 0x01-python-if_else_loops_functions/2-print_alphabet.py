fde320845b63dd9c29b80d9e456b654842324082
root <asatechafrica@gmail.com>
2024-01-04 08:39:58 -0800

    I sometimes suffer from insomnia and when I cant fall asleep I play what i call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # Project name: 2-print_alphabet.py
 # Author: sirsamuelayodele of ASA
-"""Print the alphabet in lowercase, not followed by a new line."""
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for x in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(x), end='')
